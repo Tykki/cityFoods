7105a546366810fa71d5ff5d2a934ccceac2bea8
Tykki <14112429+Tykki@users.noreply.github.com>
2023-12-11 08:53:52 -0600

    home page done

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,62 +1,17 @@
 const pImages = document.querySelectorAll('.hero-parallax img')
 const s = [2.5, 3.5, 4, 5, 6, 7, 9]
-const home = document.querySelector('#home')
-const homeElems = document.querySelectorAll('.hidden')
-const homeData = [{
-	title: 'South Loop',
-	cards: [{
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}, {
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}, {
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}]
-},{
-	title: 'North West',
-	cards: [{
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}, {
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}, {
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}]
-},{
-	title: 'Downtown',
-	cards: [{
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}, {
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}, {
-		title: 'Card Heading',
-		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
-		img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-		link: '#',
-	}]
-}]
+const placeholderImg = ['https://images.unsplash.com/photo-1608877906884-5ffef2ef9612?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1606850780554-b55ea4dd0b70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1638439430466-b2bb7fdc1d67?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1622003184404-bc0c66144534?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1467003909585-2f8a72700288?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D']
+function imgShuffler(arr=placeholderImg, index=Math.floor(Math.random() * placeholderImg.length)) {
+	if (index < 0 || index >= arr.length) {
+	  // Index out of bounds, handle accordingly
+	  return null;
+	}
+  
+	// Use splice to remove the element at the specified index
+	const removedValue = arr.splice(index, 1)[0];
+  
+	return removedValue;
+  }
 // Add a scroll event to the page
 window.addEventListener('scroll', ()=>{
 // Move the imgs to the top by adding the scrolled
@@ -75,7 +30,101 @@ pImages.forEach((img, ind) => {
 // pImages[5].style.top = '-' + (window.scrollY / 7) + 'px'
 // pImages[6].style.top = '-' + (window.scrollY / 9) + 'px'
 })
+const home = document.querySelector('#home')
+const homeData = [{
+	title: 'South Loop',
+	cards: [{
+		title: 'Low Country',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: 'https://static.wixstatic.com/media/583af2_f5e7d6b184c5473aac96107bb7ca441d~mv2.jpg/v1/fill/w_980,h_630,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/583af2_f5e7d6b184c5473aac96107bb7ca441d~mv2.jpg',
+		link: '#country',
+	}, {
+		title: 'Coming Soon',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: imgShuffler(),
+		link: '#coming-soon',
+	}, {
+		title: 'Coming Soon',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: imgShuffler(),
+		link: '#coming-soon',
+	}]
+},{
+	title: 'North West',
+	cards: [{
+		title: 'Chesa\'s Bistro & Bar',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: 'https://static.wixstatic.com/media/dcde64_5ed6384849c9490899c5a7e1db2135fa~mv2.jpg/v1/fill/w_506,h_314,al_c,q_80,usm_0.66_1.00_0.01/dcde64_5ed6384849c9490899c5a7e1db2135fa~mv2.jpg',
+		link: '#chesas',
+	}, {
+		title: 'Coming Soon',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: imgShuffler(),
+		link: '#coming-soon',
+	}, {
+		title: 'Coming Soon',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: imgShuffler(),
+		link: '#coming-soon',
+	}]
+},{
+	title: 'Downtown',
+	cards: [{
+		title: 'RPM Steak',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: 'https://photos.singleplatform.com/0080360655bb1c414ce1f4b3cfa7af0eb54abcc6.jpg',
+		link: '#steak',
+	}, {
+		title: 'Coming Soon',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: imgShuffler(),
+		link: '#coming-soon',
+	}, {
+		title: 'Coming Soon',
+		preview: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.',
+		img: imgShuffler(),
+		link: '#coming-soon',
+	}]
+}]
 
+
+
+homeData.forEach((data) => {
+
+home.innerHTML += `
+	<section class="home-item hidden">
+					<h1>${data.title}</h1>
+					<div class="cards">
+					</div>
+				</section>
+	`
+})
+const homeCards = document.querySelectorAll('.cards')
+ homeData.forEach((data, indx) => {
+	 data.cards.forEach((card, ind) => {
+		 
+	 
+
+ 	homeCards[indx].innerHTML += `
+		
+ 		<section class="card hidden">
+ 			<img src="${data.cards[ind].img}" alt="">
+ 			<div class="card-content">
+ 				<h2>
+ 				${data.cards[ind].title}
+ 				</h2>
+ 				<p>
+ 				${data.cards[ind].preview}
+ 				</p>
+ 				<a href="${data.cards[ind].link}" class="button">
+ 				${'<h3>Read More</h3>'} 
+ 				</a>
+ 			</div>
+ 		</section>
+ 	`
+	})
+ })
+const homeElems = document.querySelectorAll('.hidden')
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach(entry => {
 		console.log(entry)
@@ -89,34 +138,3 @@ const observer = new IntersectionObserver((entries) => {
 
 
 homeElems.forEach(el => observer.observe(el))
-homeData.forEach((data) => {
-
-home.innerHTML += `
-	<section class="home-item hidden">
-					<h1>${data.title}</h1>
-					<div class="cards">
-					</div>
-				</section>
-	`
-})
-const homeCards = document.querySelectorAll('.cards')
-// homeData.forEach((data, ind) => {
-
-// 	homeCards[ind].innerHTML += `
-		
-// 		<section class="card hidden">
-// 			<img src="${data.cards[ind].img} alt="">
-// 			<div class="card-content">
-// 				<h2>
-// 				${data.cards[ind].title}
-// 				</h2>
-// 				<p>
-// 				${data.cards[ind].preview}
-// 				</p>
-// 				<a href="${data.cards[ind].link}" class="button">
-// 				Find out more 
-// 				</a>
-// 			</div>
-// 		</section>
-// 	`
-// })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,45 @@
 import './home'
+
+const hide = () => {
+	splashPage.classList.add('hide')
+	eventPage.classList.add('hide')
+  lore.classList.add('hide')
+  agaric.classList.add('hide')
+  works.classList.add('hide')
+	artistPage.classList.add('hide')
+	testiPage.classList.add('hide')
+	galleryPage.classList.add('hide')
+
+	navBar.forEach(link => {
+		link.classList.remove('active')
+	})
+}
+
+const hashHandler = (hash) => {
+    if (!hash) {
+     return
+    }
+    hide()
+    navBar.forEach(link => link.getAttribute('href') === hash ? link.classList.add('active'): false)
+     if (hash === "#event"){
+         eventPage.classList.remove('hide')
+     } else if (hash === "#lore"){
+         lore.classList.remove('hide')
+     } else if (hash === "#agaric"){
+         agaric.classList.remove('hide')
+     } else if (hash === "#artist"){
+         artistPage.classList.remove('hide')
+     } else if (hash === "#works"){
+         works.classList.remove('hide')
+     } else if (hash === "#testi"){
+         testiPage.classList.remove('hide')
+     } else if (hash === "#gallery"){
+         galleryPage.classList.remove('hide')
+     } else {
+     location.hash = ''
+     location.reload()
+   }
+ }
+ 
+ window.addEventListener('hashchange', (e) => hashHandler(e.target.location.hash))
+ hashHandler(window.location.hash)
